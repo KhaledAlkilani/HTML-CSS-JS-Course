eced141f76257a7647780de834b93bacd4f659e7
Khaled <khaledkelany@gmail.com>
2022-10-27 17:15:04 +0300

    40min left

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
   $( function() {
     $( "#inputCheckIn" ).datepicker();
     $( "#inputCheckOut" ).datepicker();
+
+    $('.level-slider').slick({
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrow: false,
+    });
+
   } );
