2cc7fc9a01b32e96ffce1a94a68d52a01bff44d5
Khaled <khaledkelany@gmail.com>
2022-10-27 23:28:35 +0300

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,26 @@
         arrow: false,
     });
 
+    function videoToggle() {
+      video = $('.level-video').get(0)
+
+      if (video.paused) {
+          video.play();
+          $('.video-control-play').hide();
+          $('.video-control-pause').show();
+      } else {
+          video.pause();
+          $('.video-control-play').show();
+          $('.video-control-pause').hide();
+      }
+    }
+
+    $('.video-control-play').click(function(){
+      videoToggle();
+    })
+
+    $('.video-control-pause').click(function(){
+      videoToggle();
+    })
+
   } );
